ef8e281a708e07dfb31e1efa3324f596453af7e8
Manish Mishra <manish.mishra@tum.de>
2018-04-02 18:40:07 +0200

    display the compalains in tabular format

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var json2html = require('node-json2html');
 
 var path = require('path')
 
@@ -32,10 +33,13 @@ router.get('/complainList', function(req, res) {
     var db = req.db;
     var db = req.db;
     var collection = db.get('complains');
+
     collection.find({},{},function(e,docs){
-        res.json(docs);
+		var tarns = {"<>":"div","html":"<head><style> table, th, td {border: 1px solid black;border-collapse: collapse;} th, td { padding: 5px;text-align: left;} </style></head><table><tr><th>First Name</th><th>Last Name</th><th>Gaurdian Name<th></th><th>Phone Number</th><th>Aadhar Number</th><th>Country</th><th>Subject</th></tr> <tr><td>${firstname}</td><td>${lastname}</td><td>${guardianname}<td></td><td>${Phonenumber}</td><td>${Aadharnumber}</td><td>${country}</td><td>${subject}</td></tr></table><br>"};
+		var html = json2html.transform(docs,tarns);
+		res.write(html);
+		res.end();
     });
 })
-
 
 module.exports = router;
